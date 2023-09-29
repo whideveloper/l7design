ea644929ce69829d46918b8f5ff7228b43db27b2
Front-end-4 <frontend4@hoom.com.br>
2023-09-29 16:25:54 -0300

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -16,7 +16,7 @@ return [
     'reset' => 'Your password has been reset!',
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Por favor verifique seu e-mail antes de tentar novamente.',
-    'token' => 'This password reset token is invalid.',
+    'token' => 'Este token de redefinição de senha é inválido.',
     'user' => "We can't find a user with that email address.",
 
 ];
