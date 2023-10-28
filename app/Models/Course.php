697890d1679014b9b48bed59ae30070a635625cd
Wagner Oliveira <waggner.dev@gmail.com>
2023-10-28 09:36:32 -0300

    Create model, controller and migration course

---
+++ app/Models/Course.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'subject_id',
+        'title',
+        'text',
+        'slug',
+        'path_image',
+        'link_youtube',
+        'link_vimeo',
+        'video',
+        'active',
+        'sorting',
+    ];
+
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    public function scopeActive($query){
+        return $query->where('active', 1);
+    }
+
+    public function scopeSorting($query){
+        return $query->orderBy('sorting', 'ASC');
+    }
+}
