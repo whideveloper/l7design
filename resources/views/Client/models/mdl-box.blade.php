746eff371d6a46000f39f9c9983f21e2e7a76ae1
Wagner Dev <waggner.dev@gmail.com>
2024-04-11 21:59:07 -0300

    especialidade

---
+++ resources/views/Client/models/mdl-box.blade.php
@@ -0,0 +1,15 @@
+<article class="mdl-box">
+    <div class="mdl-box__content">
+        <img src="{{ $image }}" alt="" class="mdl-box__left">
+        <div class="mdl-box__right">
+            <h3 class="mdl-box__title">{{ $title }}</h3>
+            <span class="mdl-box__date">{{ $date }}</span>
+            <span class="mdl-box__function">{{ $funcao }}</span>
+            <span class="mdl-box__crm">{{ $crm }}</span>
+            <p class="mdl-box__text">{{ $text }}</p>
+        </div>
+        <div class="mdl-box__btn">
+            <a href="{{$link}}" class="more">{{ $btnName }}</a>
+        </div>
+    </div>
+</article>
--- resources/views/Client/pages/especialidades.blade.php
+++ resources/views/Client/pages/especialidades.blade.php
@@ -1,4 +1,35 @@
 @extends('Client.core.main')
 @section('content')
-    
+    <section class="especialidades"> 
+        <div class="especialidades__content">
+            <h3 class="especialidades__title">Especialidades</h3>
+            <p class="especialidades__text">O projeto TeleNordeste conta com um time de especialistas para apoiá-los com as teleinterconsultas e teleconsultorias. Abaixo você pode conhecê-lo um pouco mais:</p>
+            
+            <div class="especialidades__categories">
+                <ul>
+                    <li><a href="">Cardiologia</a></li>
+                    <li><a href="">Endocrinologia</a></li>
+                    <li><a href="">Psiquiatria adulto</a></li>
+                    <li><a href="">Psiquiatria infantil</a></li>
+                    <li><a href="">Neurologia adulto</a></li>
+                </ul>
+            </div>
+            @php
+                $content = [
+                    'title' => 'Lucas Deporon Toldo',
+                    'date' => '',
+                    'funcao' => 'Cardiologista',
+                    'crm' => 'CRM: 0000000',            
+                    'image' => asset('Client/assets/images/doctor-image.png'),
+                    'text' => 'Sexta-feira das 9h00 às 11h00 A partir de 18 anos',
+                    'link' => '/servicos',
+                    'btnName' => 'Ver perfil completo',
+                ];
+            @endphp
+        
+            @for ($i = 0; $i < 9; $i++)
+                @include('Client.models.mdl-box', $content)
+            @endfor
+        </div>
+    </section>
 @endsection
