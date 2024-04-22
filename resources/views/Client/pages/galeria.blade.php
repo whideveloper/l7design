aed9558f94031804bbdf664654d5cc0260d1d29d
Wagner Dev <waggner.dev@gmail.com>
2024-04-22 20:52:54 -0300

    hover galeria

--- resources/views/Client/pages/galeria.blade.php
+++ resources/views/Client/pages/galeria.blade.php
@@ -10,41 +10,49 @@
                 <div class="galeria__item">
                     <a href="{{route('galeria-interna')}}" class="link-full"></a>
                     <h2 class="galeria__title">Evento</h2>
-                    <div class="galeria__image">
-                        <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
-                    </div>
-                    <div class="galeria__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
-                    </div>
-                </div>
-                <div class="galeria__item">
-                    <a href="{{route('galeria-interna')}}" class="link-full"></a>
-                    <h2 class="galeria__title">Evento</h2>
-                    <div class="galeria__image">
-                        <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
-                    </div>
-                    <div class="galeria__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                    <div class="description">
+                        <div class="galeria__image">
+                            <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
+                        </div>
+                        <div class="galeria__text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                        </div>
                     </div>
                 </div>
                 <div class="galeria__item">
                     <a href="{{route('galeria-interna')}}" class="link-full"></a>
                     <h2 class="galeria__title">Evento</h2>
-                    <div class="galeria__image">
-                        <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
-                    </div>
-                    <div class="galeria__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                    <div class="description">
+                        <div class="galeria__image">
+                            <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
+                        </div>
+                        <div class="galeria__text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                        </div>
                     </div>
                 </div>
                 <div class="galeria__item">
                     <a href="{{route('galeria-interna')}}" class="link-full"></a>
                     <h2 class="galeria__title">Evento</h2>
-                    <div class="galeria__image">
-                        <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
+                    <div class="description">
+                        <div class="galeria__image">
+                            <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
+                        </div>
+                        <div class="galeria__text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                        </div>
                     </div>
-                    <div class="galeria__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                </div>
+                <div class="galeria__item">
+                    <a href="{{route('galeria-interna')}}" class="link-full"></a>
+                    <h2 class="galeria__title">Evento</h2>
+                    <div class="description">
+                        <div class="galeria__image">
+                            <img src="{{asset('Client/assets/images/galeria-1.jpg')}}" alt="imagem galeria">
+                        </div>
+                        <div class="galeria__text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et ex vel ligula aliquam pharetra. Morbi id quam eget elit convallis sodales. Mauris imperdiet erat id velit porttitor pretium.</p>
+                        </div>
                     </div>
                 </div>
             </div>
