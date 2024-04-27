322aff937bf0542f4851af164fccc9d2035240c9
Wagner Dev <waggner.dev@gmail.com>
2024-04-26 22:22:26 -0300

    contato

--- resources/views/Client/pages/contato.blade.php
+++ resources/views/Client/pages/contato.blade.php
@@ -1,4 +1,20 @@
 @extends('Client.core.main')
 @section('content')
-    
+    <section class="contato">
+        <div class="contato__content">
+            <h2 class="contato__title">Contatos TeleNordeste</h2>
+
+            <div class="contato__list">
+                <div class="contato__item">
+                    <div class="contato__image">
+                        <img src="{{asset('Client/assets/images/cont-1.jpg')}}" alt="Adriana Miyauchi" title="Adriana Miyauchi">
+                    </div>
+                    <div class="contato__description">
+                        <p>Adriana Miyauchi</p>
+                        <span>adriana.miyauchi@haoc.com.br</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
