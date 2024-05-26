0236d508708aaf954238dd4525f0c2170df26ac2
Front-end-4 <whiwebdealtainspiracao@gmail.com>
2024-05-25 22:10:38 -0300

    ajuste rolagem

--- public/Client/assets/js/jquery.menusidebar.js
+++ public/Client/assets/js/jquery.menusidebar.js
@@ -1,3 +1,32 @@
+// function sidebar() {
+//     $('[data-sidebar]').each(function() {
+//         var alvoSidebar = $(this).attr('data-sidebar');
+//         $(alvoSidebar).addClass('sidebar');
+//         $(alvoSidebar).prepend('<div class="closeSidebar"> <span></span> <span></span> <span></span> </div>');
+//         $(this).click(function() {
+//             $(alvoSidebar).addClass('aberto');
+//             $('body').addClass('no-scroll');
+//             if (!$('.fundo-sidebar').length) { // evita que crie mais de 1 fundo-sidebar
+//                 $('body').prepend('<div class="fundo-sidebar closeSidebar"></div>');
+//             }
+//         });
+
+//         $('body').delegate('.closeSidebar', 'click', function() {
+//             $(alvoSidebar).removeClass('aberto');
+//             $('body').removeClass('no-scroll');
+//             $('.fundo-sidebar').remove();
+//         });
+
+//         // Fecha o sidebar apenas quando um item específico do submenu é clicado
+//         $('body').delegate('.sidebar .submenu__item', 'click', function(event) {
+//             $(alvoSidebar).removeClass('aberto');
+//             $('body').removeClass('no-scroll');
+//             $('.fundo-sidebar').remove();
+//             event.stopPropagation();
+//         });
+//     });
+// }
+
 function sidebar() {
     $('[data-sidebar]').each(function() {
         var alvoSidebar = $(this).attr('data-sidebar');
@@ -22,6 +51,18 @@ function sidebar() {
             $(alvoSidebar).removeClass('aberto');
             $('body').removeClass('no-scroll');
             $('.fundo-sidebar').remove();
+            
+            // Redireciona para a página correspondente e rola para a seção
+            var targetSection = $(this).attr('data-target-section');
+            if (targetSection) {
+                window.location.href = targetSection;
+                setTimeout(function() {
+                    $('html, body').animate({
+                        scrollTop: $(targetSection).offset().top
+                    }, 500);
+                }, 500); // Aguarde 500ms antes de rolar para a seção
+            }
+            
             event.stopPropagation();
         });
     });
