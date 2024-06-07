388c2d52c7a4c5bb450b7b2bac30824bd7caaa43
Front-end-4 <whiwebdealtainspiracao@gmail.com>
2024-06-07 18:55:22 -0300

    inserção iframe grafico desempenho

--- resources/views/Client/pages/desempenho.blade.php
+++ resources/views/Client/pages/desempenho.blade.php
@@ -16,13 +16,13 @@
         </div>
 
         <div class="desempenho__grafico">
-            <img src="{{asset('Client/assets/images/desempenho.jpg')}}" alt="Desempenho">
-            {{-- <iframe width="1920" height="1080" 
+            {{-- <img src="{{asset('Client/assets/images/desempenho.jpg')}}" alt="Desempenho"> --}}
+            <iframe width="100%" height="1280" 
             src=https://lookerstudio.google.com/embed/reporting/bd0121d7-cb44-4ad5-9574-0b1b02e086dc/page/rTWzD 
             frameborder="0" 
             style="border:0" 
             allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox">
-            </iframe> --}}
+            </iframe>
         </div>
     </div>
 </section>
