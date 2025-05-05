9d502df2a6f0347b7210ee5e6df44f1b7f199cf0
Front-end-4 <whiwebdealtainspiracao@gmail.com>
2025-05-05 16:32:19 -0300

    Update panel.php

--- routes/panel.php
+++ routes/panel.php
@@ -439,9 +439,9 @@ Route::prefix('painel/')->group(function () {
         Route::post('import', [DataGraphController::class, 'import'])->name('admin.dashboard.importExcel');
         Route::get('quadro-geral-por-ubs', [DataGraphController::class, 'index'])->name('admin.dashboard.importExcel.index');
         //DESEMPENHO
-        Route::resource('titulo-sessao-desempenho', SectionTitlePerformanceController::class)
+        Route::resource('titulo-sessao-performance', SectionTitlePerformanceController::class)
             ->names('admin.dashboard.sectionTitlePerformance')
-            ->parameters(['titulo-sessao-desempenho' => 'sectionTitlePerformance']);
+            ->parameters(['titulo-sessao-performance' => 'sectionTitlePerformance']);
 
         // LOGOUT
         Route::get('logout', [AuthController::class, 'logout'])->name('admin.dashboard.user.logout');
